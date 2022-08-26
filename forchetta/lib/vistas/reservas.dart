f32f2867a85edffe5124d543998828b669db7111
alejo <luis.londono@upb.edu.co>
2022-08-25 21:01:00 -0500

    Version 2

--- forchetta/lib/vistas/reservas.dart
+++ forchetta/lib/vistas/reservas.dart
@@ -2,37 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
-class Reservas extends StatelessWidget {
-  Reservas({Key? key}) : super(key: key);
-
-  double screenH = 0.0, screenW = 0.0;
+class Reservas extends StatefulWidget {
+  const Reservas({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    screenH = MediaQuery.of(context).size.height;
-    screenW = MediaQuery.of(context).size.width;
+  State<Reservas> createState() => _Reservas();
+}
 
-    return Scaffold(
-      body: Center(
-        child: Container(
-          width: screenW * 0.95,
-          height: screenH * 0.89,
-          decoration: BoxDecoration(
-              color: Color(0xFFFFEED5),
-              borderRadius: BorderRadius.circular(10)),
-        ),
-      ),
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(50.0),
-        child: AppBar(
-          backgroundColor: Color(0xFFF3A05B),
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back_ios_new),
-            color: Colors.black,
-            onPressed: () => Navigator.pop(context, true),
+class _Reservas extends State<Reservas> {
+  DropdownMenuItem<String> buildMenuItem(String item) => DropdownMenuItem(
+        value: item,
+        child: Text(
+          item,
+          style: TextStyle(
+            fontSize: 15,
           ),
         ),
-      ),
-    );
-  }
+      );
 }
